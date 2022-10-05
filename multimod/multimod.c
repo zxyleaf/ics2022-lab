fe3ca91504f383ea0db62f11095124f08f3398de
tracer-nju <tracer@nju.edu.cn>
2022-10-05 09:31:45 +0800

    Static hostname: zxy-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 199265614cc540909479e765da005208   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  09:31:45 up 1 min,  1 user,  load average: 0.57, 0.34, 0.13

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,74 @@
 #include <stdint.h>
 
+uint64_t Multiply(uint64_t a, uint64_t b)
+{
+    uint64_t sum = 0;
+    while (b)
+    {
+        if (b & 0x1)
+        {
+            sum = sum + a;
+        }
+        a = a << 1;
+        b = b >> 1;
+    }
+    return sum;
+}
+uint64_t Divide(uint64_t a, uint64_t b)
+{
+    uint64_t quotient = 0;
+    if (!b)
+    {
+        return 1;
+    }
+    for (int i = 63; i--; i > 0)
+    {
+        if ((a >> i) >= b)
+        {
+            quotient = quotient + (1 << i);
+            a = a - (b << i);
+        }
+    }
+
+    return quotient;
+}
+uint64_t Remainder(uint64_t a, uint64_t b) //求余位运算a%b
+{
+    uint64_t c, f;
+    c = Divide(a, b);
+    f = a - (Multiply(b, c));
+    return f;
+}
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  uint64_t result = 0, tmp;
+    tmp = Remainder(a, m);
+
+    while (b)
+    {
+        if (b & 1) //每次求b的最低位 如果是1则进
+        {
+            uint64_t ifcf = result + tmp;
+            if ((result >> 63 == 1 && tmp >> 63 == 1) || (result >> 63 == 1 && ifcf >> 63 == 0 && tmp >> 63 == 0) || (result >> 63 == 0 && ifcf >> 63 == 0 && tmp >> 63 == 1))
+            {
+                uint64_t thesub = m - tmp;
+                result = result - thesub;
+            }
+            else
+                result += tmp;
+            while (result >= m)
+                result -= m;
+        }
+        if (tmp >> 63 == 1)
+        {
+            uint64_t thesub = m - tmp;
+            tmp = tmp - thesub;
+        }
+        else
+            tmp <<= 1; //计算 A*2^n的值。
+        while (tmp >= m)
+            tmp -= m;
+        b >>= 1;
+    }
+    return result;
 }

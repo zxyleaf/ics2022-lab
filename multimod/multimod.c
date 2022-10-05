bfa131f31880c123e42ea8dc8b96e5d41757265a
tracer-nju <tracer@nju.edu.cn>
2022-10-05 17:42:13 +0800

    Static hostname: zxy-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 03c6301bf17e4710b2bc5688cd7a35de   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:42:14 up 36 min,  1 user,  load average: 0.00, 0.00, 0.01

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -62,7 +62,7 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
         if (b & 1) //每次求b的最低位 如果是1则进
         {
             uint64_t ifcf = result + tmp;
-            if ((result >> 1) + (tmp >> 1) >= (-1ULL >> 1))
+            if ((result + tmp) <= tmp || (result + tmp) <= result)
             {
                 uint64_t thesub;
                 thesub = m - tmp;

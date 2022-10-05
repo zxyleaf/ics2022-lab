481cdc298ce61fe3fbeb8ed2525879ccd65db206
tracer-nju <tracer@nju.edu.cn>
2022-10-05 17:35:55 +0800

    Static hostname: zxy-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 03c6301bf17e4710b2bc5688cd7a35de   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:35:55 up 30 min,  1 user,  load average: 0.06, 0.04, 0.05

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -72,7 +72,7 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
                 result = result + tmp;
             result = Remainder(result, m);
         }
-        if ((tmp >> 1) + (tmp >> 1) >= (-1ULL >> 1))
+        if ((tmp + tmp < tmp)
         {
             uint64_t thesub;
             thesub = m - tmp;

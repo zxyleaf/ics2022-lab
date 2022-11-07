75ee0be0ed3843964a141c63a1316428f68f224c
tracer-nju <tracer@nju.edu.cn>
2022-11-07 15:16:40 +0800

    Static hostname: zxy-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 2c0d2d78d3c84d049701a5db42f4620e   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:16:40 up 2 min,  1 user,  load average: 0.18, 0.19, 0.08

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -12,21 +12,65 @@ int64_t asm_add(int64_t a, int64_t b) {
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
-  return s;
+  int ans = 0;
+    asm(
+        "movq %1, -0x18(%%rbp)\n"
+        "movl $0x0, -0x8(%%rbp)\n" // i
+        "themain:\n"
+        "cmpl $0x3f, -0x8(%%rbp)\n"
+        "jge e0\n" // 63小则跳转
+        "mov -0x8(%%rbp), %%eax\n"
+        "movq -0x18(%%rbp), %%rdx\n"
+        "mov %%eax, %%ecx\n"
+        "shr %%cl, %%rdx\n"
+        "movq %%rdx, %%rax\n"
+        "and $0x1, %%eax\n"
+        "cmp $0x0,%%rax\n" // 如果rax为零，设置ZF零标志为1   "cmp %%eax $0\n\t"
+        "je e1\n"
+        "addl $0x1, %0\n"
+        "e1:\n"
+        "addl $0x1, -0x8(%%rbp)\n"
+        "jmp themain\n"
+        "e0:\n"
+        : "+r"(ans)
+        : "r"(x)
+        : "%rax", "%eax", "%ecx", "%rdx", "%cl");
+    return ans;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
+  void *pos = dest;
+    asm("mov %0, -0x18(%%rbp)\n" // dest
+        "mov %1, -0x20(%%rbp)\n" // src
+        "mov %2, -0x28(%%rbp)\n" // n
+        "mov -0x18(%%rbp), %%rax\n"
+        "mov %%rax, %3\n"          //传递pos参数
+        "movl $0x0, -0xc(%%rbp)\n" // loop variable
+        "jmp start\n"
+        "loop:\n"
+        "mov -0x20(%%rbp),%%rax\n"
+        "movzbl (%%rax),%%edx\n"
+        "mov -0x18(%%rbp),%%rax\n"
+        "mov %%dl, (%%rax)\n"
+        "addq $0x1, -0x18(%%rbp)\n"
+        "addq $0x1, -0x20(%%rbp)\n"
+        "addq $0x1, -0xc(%%rbp)\n"
+        "start:\n"
+        "mov -0xc(%%rbp),%%rax\n"
+        "cltq\n"
+        "cmp %%rax, -0x28(%%rbp)\n"
+        "ja loop\n"
+        :
+        : "r"(dest), "r"(src), "r"(n), "r"(pos)
+        : "%rax", "%edx", "%rdx", "%dl");
+    // printf("%s\n",(char*)pos);
+    return pos;
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  return setjmp(env);
+  return 0;
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  longjmp(env, val);
+  return;
 }

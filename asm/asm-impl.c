cc6d64e04f896afc5b08cca283da612c64977ce2
tracer-nju <tracer@nju.edu.cn>
2022-11-07 16:35:24 +0800

    Static hostname: zxy-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 2c0d2d78d3c84d049701a5db42f4620e   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:35:24 up 38 min,  1 user,  load average: 0.54, 0.13, 0.04

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -1,97 +1,64 @@
 #include "asm.h"
 #include <string.h>
 
-int64_t asm_add(int64_t a, int64_t b) {
-  int64_t c = 0;
-  asm(
-        "add %2, %0"      
-        : "=g"(c)          
-        : "0"(a), "g"(b)    
-        : "memory");        
-  return c;
+int64_t asm_add(int64_t a, int64_t b)
+{
+    int64_t c = 0;
+    asm(
+        "add %2, %0"
+        : "=g"(c)
+        : "0"(a), "g"(b)
+        : "memory");
+    return c;
 }
 
-int asm_popcnt(uint64_t x) {
-  int ans;
-  asm(
-  "movl $0, %%eax;"//ans
-  "movl $0, %%ecx;"//i
-  "cycle_popcnt:cmpl $64, %%ecx;"
-  "jge label;"
-  "movq %%rbx, %%rdx;"
-  "shrq %%ecx, %%rdx;"
-  "and $1, %%rdx;"
-  "cmpq $1, %%rdx;"
-  "je t1;"
-  "jmp t2;"
-  "t1:"
-  "incl %%eax;"
-  "t2:"
-  "incl %%ecx;"
-  "jmp cycle_popcnt;"
-  "label:"
-  :"=a"(ans)
-  :"b"(x));
-  return ans;
+int asm_popcnt(uint64_t x)
+{
+    int ans;
+    asm(
+        "movl $0, %%eax;"       // int ans = 0
+        "movl $0, %%ecx;"       // int i = 0
+        "loop:cmpl $64, %%ecx;" // for (i < 64)
+        "jge e;"
+        "movq %%rbx, %%rdx;" //  rdx -- x
+        "shrq %%ecx, %%rdx;" // rdx -- x >> i
+        "and $1, %%rdx;"     // rdx -- x = x & 1
+        "cmpq $1, %%rdx;"    // rdx -- x == 0 ?
+        "je t1;"
+        "jmp t2;"
+        "t1:"
+        "incl %%eax;" // ans++
+        "t2:"
+        "incl %%ecx;" // i++
+        "jmp loop;"
+        "e:"
+        : "=a"(ans) // eax -- ans
+        : "b"(x));  // rbx -- x
+    return ans;
 }
 
-void *asm_memcpy(void *dest, const void *src, size_t n) {
-  void *pos = dest;
-    asm("mov %0, -0x18(%%rbp)\n" // dest
-        "mov %1, -0x20(%%rbp)\n" // src
-        "mov %2, -0x28(%%rbp)\n" // n
-        "mov -0x18(%%rbp), %%rax\n"
-        "mov %%rax, %3\n"          //传递pos参数
-        "movl $0x0, -0xc(%%rbp)\n" // loop variable
-        "jmp start\n"
-        "loop:\n"
-        "mov -0x20(%%rbp),%%rax\n"
-        "movzbl (%%rax),%%edx\n"
-        "mov -0x18(%%rbp),%%rax\n"
-        "mov %%dl, (%%rax)\n"
-        "addq $0x1, -0x18(%%rbp)\n"
-        "addq $0x1, -0x20(%%rbp)\n"
-        "addq $0x1, -0xc(%%rbp)\n"
-        "start:\n"
-        "mov -0xc(%%rbp),%%rax\n"
-        "cltq\n"
-        "cmp %%rax, -0x28(%%rbp)\n"
-        "ja loop\n"
-        :
-        : "r"(dest), "r"(src), "r"(n), "r"(pos)
-        : "%rax", "%edx", "%rdx", "%dl");
-    // printf("%s\n",(char*)pos);
-    return pos;
+void *asm_memcpy(void *dest, const void *src, size_t n)
+{
+    //void *pos = dest;
+    asm("movq $0,%%rsi;"
+        "loop: cmpq %%rsi, %%rcx;"
+        "jbe e;"
+        "mov (%%rbx,%%rsi,1),%%dl;"
+        "mov %%dl, (%%rax,%%rsi,1);"
+        "addq $0x1, %%rsi;"  //rsi ++
+        "jmp loop;"
+        "e:"
+        : "=a"(dest)
+        : "a"(dest), "b"(src), "c"(n));
+    return dest;
 }
 
-int asm_setjmp(asm_jmp_buf env) {
-  void *pos = dest;
-    asm("mov %0, -0x18(%%rbp)\n" // dest
-        "mov %1, -0x20(%%rbp)\n" // src
-        "mov %2, -0x28(%%rbp)\n" // n
-        "mov -0x18(%%rbp), %%rax\n"
-        "mov %%rax, %3\n"          //传递pos参数
-        "movl $0x0, -0xc(%%rbp)\n" // loop variable
-        "jmp start\n"
-        "loop:\n"
-        "mov -0x20(%%rbp),%%rax\n"
-        "movzbl (%%rax),%%edx\n"
-        "mov -0x18(%%rbp),%%rax\n"
-        "mov %%dl, (%%rax)\n"
-        "addq $0x1, -0x18(%%rbp)\n"
-        "addq $0x1, -0x20(%%rbp)\n"
-        "addq $0x1, -0xc(%%rbp)\n"
-        "start:\n"
-        "mov -0xc(%%rbp),%%rax\n"
-        "cltq\n"
-        "cmp %%rax, -0x28(%%rbp)\n"
-        "ja loop\n"
-        :
-        : "r"(dest), "r"(src), "r"(n), "r"(pos)
-        : "%rax", "%edx", "%rdx", "%dl");
-  return pos;
+int asm_setjmp(asm_jmp_buf env)
+{
+    return 0;
 }
 
-void asm_longjmp(asm_jmp_buf env, int val) {
-  return;
+void asm_longjmp(asm_jmp_buf env, int val)
+{
+    return;
 }

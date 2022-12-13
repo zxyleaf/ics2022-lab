ef0c579c8b98b3493163e016324b47916782ec3e
tracer-nju <tracer@nju.edu.cn>
2022-12-13 11:46:49 +0800

    Static hostname: zxy        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 3350043d72144297ab649796b5c7b61b   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  11:46:49 up  9:01,  7 users,  load average: 0.00, 0.02, 0.00

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include <inttypes.h>
 #include <string.h>
+
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
@@ -11,12 +12,14 @@ static int CACHE_GROUP_WIDTH;
 typedef struct{
   bool dirty_bit : 1;
   bool valid_bit : 1;
-  uint32_t tag;
+  uint32_t tag : 13;
   uint8_t data[BLOCK_SIZE];
 }cache_line;
 
 static cache_line **cache;
 
+static uint64_t hit_num = 0;
+static uint64_t miss_num = 0;
 static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
@@ -24,10 +27,84 @@ void cycle_increase(int n) { cycle_cnt += n; }
 // TODO: implement the following functions
 
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  addr &= ~0x3;
+  uint32_t block_addr = addr & (BLOCK_SIZE - 1);
+  uint32_t group_id = (addr >> BLOCK_WIDTH) & (CACHE_GROUP_NUM - 1);
+  uint32_t tag = addr >> (BLOCK_WIDTH + CACHE_GROUP_WIDTH);
+  uint32_t block_num  = addr >> (BLOCK_WIDTH);
+  uint32_t *ret;
+
+  for (int i = 0; i < CACHE_LINE_NUM; i++)
+  {
+    if (cache[group_id][i].valid_bit && cache[group_id][i].tag == tag)
+    {
+      hit_num++;
+      ret = (void *)(cache[group_id][i].data + (block_addr));
+      return *ret;
+    }
+  }
+
+  miss_num++;
+  for (int i = 0; i < CACHE_LINE_NUM; i++)
+  {
+    if (cache[group_id][i].valid_bit == false)
+    {
+      mem_read(block_num, cache[group_id][i].data);
+      cache[group_id][i].valid_bit = true;
+      cache[group_id][i].tag = tag;
+      ret = (void *)(cache[group_id][i].data + (block_addr));
+      return *ret;
+    }
+  }
+
+  int line = rand() % CACHE_LINE_NUM;
+  mem_read(block_num, cache[group_id][line].data);
+  cache[group_id][line].valid_bit = true;
+  cache[group_id][line].tag = tag;
+  ret = (void *)cache[group_id][line].data + (block_addr);
+  return *ret;
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  addr &= ~0x3;
+  uint32_t block_addr = addr & (BLOCK_SIZE - 1);
+  uint32_t group_id = (addr >> BLOCK_WIDTH) & (CACHE_GROUP_NUM - 1);
+  uint32_t tag = addr >> (BLOCK_WIDTH + CACHE_GROUP_WIDTH);
+  uint32_t block_num  = addr >> (BLOCK_WIDTH);
+  for (int i = 0; i < CACHE_LINE_NUM; i++)
+  {
+    if (cache[group_id][i].valid_bit && cache[group_id][i].tag == tag)
+    {
+      uint32_t *tmp =(void *)(cache[group_id][i].data + (addr % BLOCK_SIZE));
+      *tmp=(*tmp & ~wmask) | (data & wmask);
+      mem_write(block_num, cache[group_id][i].data);
+      return;
+    }
+  }
+   miss_num++;
+  for (int i = 0; i < CACHE_LINE_NUM; i++)
+  {
+    if (cache[group_id][i].valid_bit == false)
+    {
+      mem_read(block_num, cache[group_id][i].data);
+
+      cache[group_id][i].tag = tag;
+      cache[group_id][i].valid_bit = true;
+      uint32_t *tmp =(void *)(cache[group_id][i].data + (addr % BLOCK_SIZE));
+      *tmp = (*tmp & ~wmask) | (data & wmask);
+      mem_write(block_num, cache[group_id][i].data);
+      return ;
+    }
+  }
+
+  int line = rand() % CACHE_LINE_NUM;
+  mem_read(block_num, cache[group_id][line].data);
+  cache[group_id][line].tag = tag;
+  uint32_t *tmp = (void *)(cache[group_id][line].data + (addr % BLOCK_SIZE));
+  *tmp = (*tmp & ~wmask) | (data & wmask);
+  mem_write(block_num,cache[group_id][line].data);
+  return ;
+
 }
 
 void init_cache(int total_size_width, int associativity_width) {
@@ -42,7 +119,6 @@ void init_cache(int total_size_width, int associativity_width) {
   for (int i = 0; i < CACHE_GROUP_NUM; i++){
     for (int j = 0; j < CACHE_LINE_NUM; j++){
       cache[i][j].valid_bit = 0;
-	  cache[i][j].dirty_bit = 0;
 	  memset(cache[i][j].data, 0, sizeof(cache[i][j].data));
 	}
   }

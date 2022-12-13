7b381e32169b2b577238caab35f7ca455123b576
tracer-nju <tracer@nju.edu.cn>
2022-12-13 10:23:39 +0800

    Static hostname: zxy        Icon name: computer-vm          Chassis: vm       Machine ID: e54c30f7f7dd49f29deaa24d119445e0          Boot ID: 3350043d72144297ab649796b5c7b61b   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  10:23:39 up  7:40,  7 users,  load average: 0.10, 0.07, 0.02

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -4,6 +4,19 @@
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
+static int CACHE_LINE_NUM;
+static int CACHE_GROUP_NUM;
+static int CACHE_ALL_LINE_NUM;
+static int CACHE_GROUP_WIDTH;
+typedef struct{
+  bool dirty_bit : true;
+  bool valid_bit : false;
+  uint32_t tag;
+  uint8_t data[BLOCK_SIZE];
+}cache_line;
+
+static cache_line **cache;
+
 static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
@@ -18,6 +31,21 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  CACHE_LINE_NUM = exp2(associativity_width);
+  CACHE_GROUP_NUM = (exp2(total_size_width) / BLOCK_SIZE) / CACHE_LINE_NUM;
+  CACHE_ALL_LINE_NUM = exp2(total_size_width - BLOCK_WIDTH);
+  CACHE_GROUP_WIDTH = total_size_width - BLOCK_WIDTH - associativity_width;
+  cache = (cache_line**)malloc(sizeof(cache_line*)*CACHE_GROUP_NUM);
+  for (int i = 0; i < CACHE_GROUP_NUM; i++){
+    cache[i] = (cache_line*)malloc(sizeof(cache_line)*CACHE_LINE_NUM);
+  }
+  for (int i = 0; i < CACHE_GROUP_NUM; i++){
+    for (int j = 0; j < CACHE_LINE_NUM; j++){
+      cache[i][j].valid_bit = 0;
+	  cache[i][j].dirty_bit = 0;
+	  memset(cache[i][j].data, 0, sizeof(cache[i][j].data))
+	}
+  }
 }
 
 void display_statistic(void) {
